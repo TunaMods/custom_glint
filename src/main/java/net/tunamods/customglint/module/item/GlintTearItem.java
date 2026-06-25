adcdf5d6772efe7b94aadc3ca10fc3be364326a3
Tuna <tunamodsminecraft@gmail.com>
2026-06-25 16:28:22 -0400

    remove unused imports from the glint tear items
    - GlintBlackTearItem, GlintLayerTearItem, GlintTearItem: drop the unused Level / Nullable
      imports left over from earlier signatures

--- src/main/java/net/tunamods/customglint/module/item/GlintTearItem.java
+++ src/main/java/net/tunamods/customglint/module/item/GlintTearItem.java
@@ -7,9 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class GlintTearItem extends Item {
